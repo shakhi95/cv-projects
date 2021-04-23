f5b77faf74b60b76e8be29f12c25e356f242b2dc
shakhi95 <shakhiii2019@gmail.com>
2021-04-23 15:45:40 +0430

    re-order

--- src/App.js
+++ src/App.js
@@ -47,52 +47,10 @@ const App = () => {
         </div>
       </div>
       <hr />
-      <h3 className="text-center mb-4">HTML, Pure CSS, Vanilla JS</h3>
+      <h3 className="text-center mb-4">React L-3</h3>
       <div className="row px-md-5">
         {data.map((item) => {
-          if (item.level === "js") {
-            return (
-              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
-                <ItemCard {...item} />
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <hr />
-      <h3 className="text-center mb-4">Bootstrap V5</h3>
-      <div className="row px-md-5">
-        {data.map((item) => {
-          if (item.level === "bs5") {
-            return (
-              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
-                <ItemCard {...item} />
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <hr />
-      <h3 className="text-center mb-4">React L-0</h3>
-      <div className="row px-md-5">
-        {data.map((item) => {
-          if (item.level === "react0") {
-            return (
-              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
-                <ItemCard {...item} />
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <hr />
-      <h3 className="text-center mb-4">React L-1</h3>
-      <div className="row px-md-5">
-        {data.map((item) => {
-          if (item.level === "react1") {
+          if (item.level === "react3") {
             return (
               <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
                 <ItemCard {...item} />
@@ -117,10 +75,52 @@ const App = () => {
         })}
       </div>
       <hr />
-      <h3 className="text-center mb-4">React L-3</h3>
+      <h3 className="text-center mb-4">React L-1</h3>
       <div className="row px-md-5">
         {data.map((item) => {
-          if (item.level === "react3") {
+          if (item.level === "react1") {
+            return (
+              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
+                <ItemCard {...item} />
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+      <hr />
+      <h3 className="text-center mb-4">React L-0</h3>
+      <div className="row px-md-5">
+        {data.map((item) => {
+          if (item.level === "react0") {
+            return (
+              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
+                <ItemCard {...item} />
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+      <hr />
+      <h3 className="text-center mb-4">Bootstrap V5</h3>
+      <div className="row px-md-5">
+        {data.map((item) => {
+          if (item.level === "bs5") {
+            return (
+              <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
+                <ItemCard {...item} />
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+      <hr />
+      <h3 className="text-center mb-4">HTML, Pure CSS, Vanilla JS</h3>
+      <div className="row px-md-5">
+        {data.map((item) => {
+          if (item.level === "js") {
             return (
               <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
                 <ItemCard {...item} />
